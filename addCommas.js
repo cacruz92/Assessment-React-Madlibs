96e81524faef14188088a66478f2c38996371972
Christian Cruz <christian.cruz92@hotmail.com>
2024-07-03 17:23:05 -0400

    added tests and fixed the code to make sure all tests passed for addCommas.js

--- addCommas.js
+++ addCommas.js
@@ -2,15 +2,26 @@ function addCommas(num) {
     // Convert number to a string
     let numString = num.toString();
 
+    // see if the number is negative
+    let isNegative = numString[0]=== '-';
+
+    if(isNegative){
+        numString  = numString.slice(1);
+    }
+  
     // separate each number and enter them into an array
     let numArr = numString.split('');
 
     // loop over the array backward and then insert a comma every three spaces
-    for(let i = numArr.length-1; i>0; i-=3){
+    for(let i = numArr.length-3; i>0; i-=3){
         numArr.splice(i,0,',');
     }
 
     let finalNum = numArr.join('');
+
+    if(isNegative){
+        finalNum = '-'+ finalNum;
+    }
 
     return finalNum;
 }
--- addCommas.test.js
+++ addCommas.test.js
@@ -4,4 +4,20 @@ describe("#addCommas", () => {
   test("it is a function", () => {
     expect(typeof addCommas).toBe("function");
   });
+
+  test("it changes numbers to strings with commas", ()=>{
+    expect(addCommas(1234)).toBe('1,234');
+    expect(addCommas(1000000)).toBe('1,000,000');
+    expect(addCommas(9876543210)).toBe('9,876,543,210');
+  })
+
+  test("it will correctly display a number that doesn't need commas", () => {
+    expect(addCommas(6)).toBe('6');
+  })
+
+  test("it will correctly display a negative number", () => {
+    expect(addCommas(-10)).toBe('-10');
+    expect(addCommas(-104)).toBe('-104');
+    expect(addCommas(-5678)).toBe('-5,678');
+  })
 });
